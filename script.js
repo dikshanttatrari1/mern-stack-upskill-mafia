b171e35ed084aaede7cd38bc840936aab11746c7
Dikshant Tatrari <165898250+dikshanttatrari1@users.noreply.github.com>
2024-04-17 11:47:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let a = 3;
+let b = 4;
+let c = 32;
+let d = 87;
+let e = 99;
+
+if(a%2==0){
+    console.log("The number",a, "is even. ");
+}else{
+    console.log("The number ", a, "is odd. ");
+}
+
+if(b%2==0){
+    console.log("The number",b, "is even. ");
+}else{
+    console.log("The number ", b, "is odd. ");
+}
+
+if(c%2==0){
+    console.log("The number",c, "is even. ");
+}else{
+    console.log("The number ", c, "is odd. ");
+}
+
+if(d%2==0){
+    console.log("The number",d, "is even. ");
+}else{
+    console.log("The number ", d, "is odd. ");
+}
+
+if(e%2==0){
+    console.log("The number",e, "is even. ");
+}else{
+    console.log("The number ", e, "is odd. ");
+}
